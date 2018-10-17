90cedb7dd7df852ad8222bfecb06f02983b7b5b5
fxaps <andrew.rajcany@aus.fujixerox.com>
2018-10-17 17:50:55 +1100

    Backup Script

---
+++ Export_FFC_Worflows.bat
@@ -0,0 +1,40 @@
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::
+:: FreeFlow Core Backup Script
+::
+:: Note:
+:: You may need to run this script as Administrator
+:: Configure the backup directory
+:: Automate this script by setting it as a target for a Windows Scheduled Task
+::
+:: @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+:: @link          https://github.com/fxaps/FreeFlowCoreScripts
+:: @version       1.0.0
+:: @author        Andrew.Rajcany@aus.fujixerox.com
+::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+
+@echo off
+echo About to Backup FFC Workflows...
+echo.
+echo.
+
+REM DEFINE THE FILE PATH FOR THE BACKUP
+set backupfilepath=c:\tmp\
+
+REM CREATE UNIQUE DATETIME STRING IN FORMAT YYYYMMDD-HHMMSS
+FOR /f %%a IN ('WMIC OS GET LocalDateTime ^| FIND "."') DO SET DTS=%%a
+SET DateTime=%DTS:~0,8%_%DTS:~8,6%
+
+REM DEFINE THE FILE NAME FOR THE BACKUP
+set backupfilename=%DateTime%_FreeFlowCore_Backup.ffc
+set backupfullpath=%backupfilepath%%backupfilename%
+
+REM PERFORM THE BACKUP
+echo %backupfullpath%
+"%FF_CORE_HOME%CoreExchange.exe" /e "%backupfullpath%" /clone /r 2>&1 "%backupfullpath%.txt"
+echo.
+echo.
+
